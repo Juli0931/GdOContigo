e5a944952f5438b3f47a360030d67825ac3f163a
Karolfc <karolfuentescaro@gmail.com>
2024-04-30 13:01:53 -0500

    Question Section

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import { IoIosHelpCircleOutline } from "react-icons/io";
 import { FaArrowRightLong } from "react-icons/fa6";
+import questionsImg from "../../assets/questions.jpg";
 
 import "./Questions.css";
 
-const frecuentlyQ = (question) => {
+const FrecuentlyQ = ({question}) => {
     return (
-        <div className="card-container">
-            <IoIosHelpCircleOutline />
+      <>
+        <div className="divQuestion">
+            <IoIosHelpCircleOutline className="helpIcon"/>
             <p>{question}</p>
-            <FaArrowRightLong />
+            <FaArrowRightLong className="arrowIcon"/>
         </div>
+      </>
       
     );
   };
@@ -18,17 +21,22 @@ const frecuentlyQ = (question) => {
 export function Questions() {
   return (
     <>
-    <section>
+    <section className="questionContainer">
         <h2>Respondemos a tus preguntas</h2>
-        <section>
-            <img src="" alt="" />
+        <section className="sectionQuestion">
+          <section>
+            <img src={questionsImg} alt="" />
             <button></button>
+          </section>
+        <section className="consultaSection">
+          <h3>Lo más consultado</h3>
+            <FrecuentlyQ question={<>¿Qué debo hacer en caso de detectar un<br></br>olor a gas en mi hogar o negocio?</>}/>
+            <FrecuentlyQ question={<>¿Qué debo hacer si pierdo mi factura de gas?</>}/>
+            <FrecuentlyQ question={<>¿Cuáles son los requisitos para solicitar un<br></br>servicio de conexión de gas natural en mi hogar?</>}/>
+            <FrecuentlyQ question={<>¿Qué debo hacer es tengo problemas con mi medidor<br></br>de gas o experimento fluctuaciones en mi suministro?</>}/>
         </section>
-        <section>
-            <h4>Lo más consultado</h4>
-            <frecuentlyQ question={<>¿Qué debo hacer en caso de detextar un<br/>olor a gas en mi casa o negocio?</>}/>
-
-        </section>
+    </section>
+    <h3>Ver todas las preguntas frecuentes</h3>
     </section>
     </>
   );
